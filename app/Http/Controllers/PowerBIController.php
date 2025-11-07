d5f6b3b3fb60094ff51edbf0b68d02ff8573ab9f
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2025-11-07 17:10:47 -0500

    cambios varios

--- app/Http/Controllers/PowerBIController.php
+++ app/Http/Controllers/PowerBIController.php
@@ -94,349 +94,41 @@ class PowerBIController extends Controller
 
     public function localizacion()
     {
+        $data = $this->getPowerBIEmbedData(
+            'f75af920-12ae-40a8-8ecd-203bd4c5d41c',
+            '048a6d69-7b16-4f19-851c-3496947d2709'
+        );
 
-        $tenantId = env('AZURE_TENANT_ID');
-        $clientId = env('AZURE_CLIENT_ID');
-        $clientSecret = env('AZURE_CLIENT_SECRET');
-        $workspaceId = env('POWER_BI_WORKSPACE_ID');
-        $reportId = 'f75af920-12ae-40a8-8ecd-203bd4c5d41c';
-
-        $reportIdMobil = '048a6d69-7b16-4f19-851c-3496947d2709';
-
-        // Obtener el access token
-       $tokenResponse = Http::asForm()
-        ->post("https://login.microsoftonline.com/{$tenantId}/oauth2/v2.0/token", [
-            'grant_type' => 'client_credentials',
-            'client_id' => $clientId,
-            'client_secret' => $clientSecret,
-            'scope' => 'https://analysis.windows.net/powerbi/api/.default',
-        ]);
-      
-
-        if (!$tokenResponse->ok() || !isset($tokenResponse->json()['access_token'])) {
-            return response()->json([
-                'error' => 'Error al obtener access token',
-                'details' => $tokenResponse->json()
-            ], 500);
-        }
-
-        $accessToken = $tokenResponse->json()['access_token'];
-
-        // Obtener detalles del reporte
-        $reportResponse = Http::withToken($accessToken)->get("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportId}");
-        
-
-        if (!$reportResponse->ok()) {
-            return response()->json([
-                'error' => 'Error al obtener reporte Power BI',
-                'details' => $reportResponse->json()
-            ], 500);
-        }
-
-        $report = $reportResponse->json();
-        $embedUrl = $report['embedUrl'];
-
-        $reportResponseMobil = Http::withToken($accessToken)->get("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportIdMobil}");
-
-        if (!$reportResponseMobil->ok()) {
-            return response()->json([
-                'error' => 'Error al obtener reporte Power BI',
-                'details' => $reportResponseMobil->json()
-            ], 500);
-        }
-
-        $reportMobil = $reportResponseMobil->json();
-        $embedUrlMobil = $reportMobil['embedUrl'];
-
-        // Generar embed token
-        $embedTokenResponse = Http::withToken($accessToken)->post("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportId}/GenerateToken", [
-            'accessLevel' => 'View'
-        ]);
-
-        if (!$embedTokenResponse->ok() || !isset($embedTokenResponse->json()['token'])) {
-            return response()->json([
-                'error' => 'Error al generar embed token',
-                'details' => $embedTokenResponse->json()
-            ], 500);
-        }
-
-        $embedToken = $embedTokenResponse->json()['token'];
-
-        // Generar embed token Mobil
-        $embedTokenResponseMobil = Http::withToken($accessToken)->post("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportIdMobil}/GenerateToken", [
-            'accessLevel' => 'View'
-        ]);
-
-        if (!$embedTokenResponseMobil->ok() || !isset($embedTokenResponseMobil->json()['token'])) {
-            return response()->json([
-                'error' => 'Error al generar embed token',
-                'details' => $embedTokenResponseMobil->json()
-            ], 500);
-        }
-
-        $embedTokenMobil = $embedTokenResponseMobil->json()['token'];
-
-        return view('localizacion', compact('reportId', 'embedUrl', 'embedToken','reportIdMobil', 'embedUrlMobil', 'embedTokenMobil'));
+        return view('localizacion', $data);
     }
 
     public function indicador()
     {
+        $data = $this->getPowerBIEmbedData(
+            '84788d15-7aec-4b6f-a6b5-afb36bda26ae',
+            '860f7180-b8a0-444e-8fe3-53f9f4afb22a'
+        );
 
-        $tenantId = env('AZURE_TENANT_ID');
-        $clientId = env('AZURE_CLIENT_ID');
-        $clientSecret = env('AZURE_CLIENT_SECRET');
-        $workspaceId = env('POWER_BI_WORKSPACE_ID');
-        $reportId = '84788d15-7aec-4b6f-a6b5-afb36bda26ae';
-
-        $reportIdMobil = '860f7180-b8a0-444e-8fe3-53f9f4afb22a';
-
-        // Obtener el access token
-       $tokenResponse = Http::asForm()
-        ->post("https://login.microsoftonline.com/{$tenantId}/oauth2/v2.0/token", [
-            'grant_type' => 'client_credentials',
-            'client_id' => $clientId,
-            'client_secret' => $clientSecret,
-            'scope' => 'https://analysis.windows.net/powerbi/api/.default',
-        ]);
-      
-
-        if (!$tokenResponse->ok() || !isset($tokenResponse->json()['access_token'])) {
-            return response()->json([
-                'error' => 'Error al obtener access token',
-                'details' => $tokenResponse->json()
-            ], 500);
-        }
-
-        $accessToken = $tokenResponse->json()['access_token'];
-
-        // Obtener detalles del reporte
-        $reportResponse = Http::withToken($accessToken)->get("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportId}");
-        
-
-        if (!$reportResponse->ok()) {
-            return response()->json([
-                'error' => 'Error al obtener reporte Power BI',
-                'details' => $reportResponse->json()
-            ], 500);
-        }
-
-        $report = $reportResponse->json();
-        $embedUrl = $report['embedUrl'];
-
-        $reportResponseMobil = Http::withToken($accessToken)->get("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportIdMobil}");
-
-        if (!$reportResponseMobil->ok()) {
-            return response()->json([
-                'error' => 'Error al obtener reporte Power BI',
-                'details' => $reportResponseMobil->json()
-            ], 500);
-        }
-
-        $reportMobil = $reportResponseMobil->json();
-        $embedUrlMobil = $reportMobil['embedUrl'];
-
-        // Generar embed token
-        $embedTokenResponse = Http::withToken($accessToken)->post("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportId}/GenerateToken", [
-            'accessLevel' => 'View'
-        ]);
-
-        if (!$embedTokenResponse->ok() || !isset($embedTokenResponse->json()['token'])) {
-            return response()->json([
-                'error' => 'Error al generar embed token',
-                'details' => $embedTokenResponse->json()
-            ], 500);
-        }
-
-        $embedToken = $embedTokenResponse->json()['token'];
-
-        // Generar embed token Mobil
-        $embedTokenResponseMobil = Http::withToken($accessToken)->post("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportIdMobil}/GenerateToken", [
-            'accessLevel' => 'View'
-        ]);
-
-        if (!$embedTokenResponseMobil->ok() || !isset($embedTokenResponseMobil->json()['token'])) {
-            return response()->json([
-                'error' => 'Error al generar embed token',
-                'details' => $embedTokenResponseMobil->json()
-            ], 500);
-        }
-
-        $embedTokenMobil = $embedTokenResponseMobil->json()['token'];
-
-        return view('indicador', compact('reportId', 'embedUrl', 'embedToken','reportIdMobil', 'embedUrlMobil', 'embedTokenMobil'));
+        return view('indicador', $data);
     }
 
     public function tickets()
     {
+        $data = $this->getPowerBIEmbedData(
+            '004e326c-d908-4ad5-9856-c1b54543f886',
+            '1f7427cb-9a15-49b5-843f-5e0727ca11bf'
+        );
 
-        $tenantId = env('AZURE_TENANT_ID');
-        $clientId = env('AZURE_CLIENT_ID');
-        $clientSecret = env('AZURE_CLIENT_SECRET');
-        $workspaceId = env('POWER_BI_WORKSPACE_ID');
-        $reportId = '004e326c-d908-4ad5-9856-c1b54543f886';
-
-        $reportIdMobil = '1f7427cb-9a15-49b5-843f-5e0727ca11bf';
-
-        // Obtener el access token
-       $tokenResponse = Http::asForm()
-        ->post("https://login.microsoftonline.com/{$tenantId}/oauth2/v2.0/token", [
-            'grant_type' => 'client_credentials',
-            'client_id' => $clientId,
-            'client_secret' => $clientSecret,
-            'scope' => 'https://analysis.windows.net/powerbi/api/.default',
-        ]);
-      
-
-        if (!$tokenResponse->ok() || !isset($tokenResponse->json()['access_token'])) {
-            return response()->json([
-                'error' => 'Error al obtener access token',
-                'details' => $tokenResponse->json()
-            ], 500);
-        }
-
-        $accessToken = $tokenResponse->json()['access_token'];
-
-        // Obtener detalles del reporte
-        $reportResponse = Http::withToken($accessToken)->get("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportId}");
-        
-
-        if (!$reportResponse->ok()) {
-            return response()->json([
-                'error' => 'Error al obtener reporte Power BI',
-                'details' => $reportResponse->json()
-            ], 500);
-        }
-
-        $report = $reportResponse->json();
-        $embedUrl = $report['embedUrl'];
-
-        $reportResponseMobil = Http::withToken($accessToken)->get("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportIdMobil}");
-
-        if (!$reportResponseMobil->ok()) {
-            return response()->json([
-                'error' => 'Error al obtener reporte Power BI',
-                'details' => $reportResponseMobil->json()
-            ], 500);
-        }
-
-        $reportMobil = $reportResponseMobil->json();
-        $embedUrlMobil = $reportMobil['embedUrl'];
-
-        // Generar embed token
-        $embedTokenResponse = Http::withToken($accessToken)->post("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportId}/GenerateToken", [
-            'accessLevel' => 'View'
-        ]);
-
-        if (!$embedTokenResponse->ok() || !isset($embedTokenResponse->json()['token'])) {
-            return response()->json([
-                'error' => 'Error al generar embed token',
-                'details' => $embedTokenResponse->json()
-            ], 500);
-        }
-
-        $embedToken = $embedTokenResponse->json()['token'];
-
-        // Generar embed token Mobil
-        $embedTokenResponseMobil = Http::withToken($accessToken)->post("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportIdMobil}/GenerateToken", [
-            'accessLevel' => 'View'
-        ]);
-
-        if (!$embedTokenResponseMobil->ok() || !isset($embedTokenResponseMobil->json()['token'])) {
-            return response()->json([
-                'error' => 'Error al generar embed token',
-                'details' => $embedTokenResponseMobil->json()
-            ], 500);
-        }
-
-        $embedTokenMobil = $embedTokenResponseMobil->json()['token'];
-
-        return view('tickets', compact('reportId', 'embedUrl', 'embedToken','reportIdMobil', 'embedUrlMobil', 'embedTokenMobil'));
+        return view('tickets', $data);
     }
 
     public function kpi()
     {
+        $data = $this->getPowerBIEmbedData(
+            '2468cf20-763e-405f-9a23-f81988fa5887',
+            '9a3435a3-ad52-4be3-bb6f-b2a314d0750e'
+        );
 
-        $tenantId = env('AZURE_TENANT_ID');
-        $clientId = env('AZURE_CLIENT_ID');
-        $clientSecret = env('AZURE_CLIENT_SECRET');
-        $workspaceId = env('POWER_BI_WORKSPACE_ID');
-        $reportId = '2468cf20-763e-405f-9a23-f81988fa5887';
-
-        $reportIdMobil = '9a3435a3-ad52-4be3-bb6f-b2a314d0750e';
-
-        // Obtener el access token
-       $tokenResponse = Http::asForm()
-        ->post("https://login.microsoftonline.com/{$tenantId}/oauth2/v2.0/token", [
-            'grant_type' => 'client_credentials',
-            'client_id' => $clientId,
-            'client_secret' => $clientSecret,
-            'scope' => 'https://analysis.windows.net/powerbi/api/.default',
-        ]);
-      
-
-        if (!$tokenResponse->ok() || !isset($tokenResponse->json()['access_token'])) {
-            return response()->json([
-                'error' => 'Error al obtener access token',
-                'details' => $tokenResponse->json()
-            ], 500);
-        }
-
-        $accessToken = $tokenResponse->json()['access_token'];
-
-        // Obtener detalles del reporte
-        $reportResponse = Http::withToken($accessToken)->get("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportId}");
-        
-
-        if (!$reportResponse->ok()) {
-            return response()->json([
-                'error' => 'Error al obtener reporte Power BI',
-                'details' => $reportResponse->json()
-            ], 500);
-        }
-
-        $report = $reportResponse->json();
-        $embedUrl = $report['embedUrl'];
-
-        $reportResponseMobil = Http::withToken($accessToken)->get("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportIdMobil}");
-
-        if (!$reportResponseMobil->ok()) {
-            return response()->json([
-                'error' => 'Error al obtener reporte Power BI',
-                'details' => $reportResponseMobil->json()
-            ], 500);
-        }
-
-        $reportMobil = $reportResponseMobil->json();
-        $embedUrlMobil = $reportMobil['embedUrl'];
-
-        // Generar embed token
-        $embedTokenResponse = Http::withToken($accessToken)->post("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportId}/GenerateToken", [
-            'accessLevel' => 'View'
-        ]);
-
-        if (!$embedTokenResponse->ok() || !isset($embedTokenResponse->json()['token'])) {
-            return response()->json([
-                'error' => 'Error al generar embed token',
-                'details' => $embedTokenResponse->json()
-            ], 500);
-        }
-
-        $embedToken = $embedTokenResponse->json()['token'];
-
-        // Generar embed token Mobil
-        $embedTokenResponseMobil = Http::withToken($accessToken)->post("https://api.powerbi.com/v1.0/myorg/groups/{$workspaceId}/reports/{$reportIdMobil}/GenerateToken", [
-            'accessLevel' => 'View'
-        ]);
-
-        if (!$embedTokenResponseMobil->ok() || !isset($embedTokenResponseMobil->json()['token'])) {
-            return response()->json([
-                'error' => 'Error al generar embed token',
-                'details' => $embedTokenResponseMobil->json()
-            ], 500);
-        }
-
-        $embedTokenMobil = $embedTokenResponseMobil->json()['token'];
-
-        return view('kpi', compact('reportId', 'embedUrl', 'embedToken','reportIdMobil', 'embedUrlMobil', 'embedTokenMobil'));
+        return view('kpi', $data);
     }
 }
--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -30,12 +30,12 @@
                     </div>
                 </div>
                 <a href="/" class="nav-item nav-link {{ Request::is('/') ? 'active' : '' }}"><i class="fa fa-tachometer-alt me-2 icolor"></i>PRINCIPAL</a>
-                <a href="/soporte" class="nav-item nav-link {{ Request::is('/soporte') ? 'active' : '' }}"><i class="fa fa-laptop me-2 icolor"></i>DETALLE</a>
-                <a href="/especialista" class="nav-item nav-link {{ Request::is('/especialista') ? 'active' : '' }}"><i class="fa fa-th me-2 icolor"></i>ESPECIALISTA</a>
-                <a href="/localizacion" class="nav-item nav-link {{ Request::is('/localizacion') ? 'active' : '' }}"><i class="fa fa-keyboard me-2 icolor"></i>LOCALIZACIÓN</a>
-                <a href="/indicador" class="nav-item nav-link {{ Request::is('/indicador') ? 'active' : '' }}"><i class="fa fa-table me-2 icolor"></i>INDICADOR</a>
-                <a href="/tickets" class="nav-item nav-link {{ Request::is('/tickets') ? 'active' : '' }}"><i class="fa fa-chart-bar me-2 icolor"></i>TICKETS</a>
-                <a href="/kpi" class="nav-item nav-link {{ Request::is('/kpi') ? 'active' : '' }}"><i class="far fa-file-alt me-2 icolor"></i>KPI</a>
+                <a href="/soporte" class="nav-item nav-link {{ Request::is('soporte') ? 'active' : '' }}"><i class="fa fa-laptop me-2 icolor"></i>DETALLE</a>
+                <a href="/especialista" class="nav-item nav-link {{ Request::is('especialista') ? 'active' : '' }}"><i class="fa fa-th me-2 icolor"></i>ESPECIALISTA</a>
+                <a href="/localizacion" class="nav-item nav-link {{ Request::is('localizacion') ? 'active' : '' }}"><i class="fa fa-keyboard me-2 icolor"></i>LOCALIZACIÓN</a>
+                <a href="/indicador" class="nav-item nav-link {{ Request::is('indicador') ? 'active' : '' }}"><i class="fa fa-table me-2 icolor"></i>INDICADOR</a>
+                <a href="/tickets" class="nav-item nav-link {{ Request::is('tickets') ? 'active' : '' }}"><i class="fa fa-chart-bar me-2 icolor"></i>TICKETS</a>
+                <a href="/kpi" class="nav-item nav-link {{ Request::is('kpi') ? 'active' : '' }}"><i class="far fa-file-alt me-2 icolor"></i>KPI</a>
             
             </div>
         </nav>
